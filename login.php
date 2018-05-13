c427ba3675ba052542a3d4c417e33d9ae53b08c2
PutraSinaga <37802827+PutraSinaga@users.noreply.github.com>
2018-05-13 20:45:21 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <link rel="stylesheet" href="login.css">
+
+</head>
+  <body>
+	<div id="container">
+		<form ACTION="index.php" method="POST" target="_self" id="loginuser">
+		<label for="name">Username:</label>
+		<input name="username" type="name" id="username">
+		<label for="username">Password:</label>
+		<input name="password" type="password" id="password">
+		<div id="lower">
+		<input type="submit" value="Masuk >>">
+		</div>
+		</form>
+	</div>
+	</body>
+</html>
