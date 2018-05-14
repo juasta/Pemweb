ed6d1ae644e38e2393c0c74f6ba67ff0d09a329e
PutraSinaga <37802827+PutraSinaga@users.noreply.github.com>
2018-05-14 12:58:01 +0700

    Update pasien.php

--- pengurus/pasien.php
+++ pengurus/pasien.php
@@ -36,38 +36,47 @@ if (isset($_SERVER['QUERY_STRING'])) {
   $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
 }
 
-if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
-
-$target = "files/";
- $target = $target . basename( $_FILES['foto']['name']);
-
-  $insertSQL = sprintf("INSERT INTO pasien (nm_pasien, j_kel, agama, alamat,tgl_lhr, usia, no_tlp, nm_kk, hub_kel) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form2")) {
+  $updateSQL = sprintf("UPDATE pasien SET nm_pasien=%s, j_kel=%s, agama=%s, alamat=%s, tgl_lhr=%s, usia=%s, nm_kk=%s, hub_kel=%s WHERE no_pasien=%s",
                        GetSQLValueString($_POST['nm_pasien'], "text"),
                        GetSQLValueString($_POST['j_kel'], "text"),
                        GetSQLValueString($_POST['agama'], "text"),
                        GetSQLValueString($_POST['alamat'], "text"),
                        GetSQLValueString($_POST['tgl_lhr'], "date"),
                        GetSQLValueString($_POST['usia'], "int"),
-                       GetSQLValueString($_POST['no_tlp'], "int"),
                        GetSQLValueString($_POST['nm_kk'], "text"),
-                       GetSQLValueString($_POST['hub_kel'], "text"));
-  mysql_select_db($database_koneksi, $koneksi);
-  $Result1 = mysql_query($insertSQL, $koneksi) or die(mysql_error());
+                       GetSQLValueString($_POST['hub_kel'], "text"),
+                       GetSQLValueString($_POST['no_pasien'], "int"));
 
-  $insertGoTo = "datapasien.php";
+  mysql_select_db($database_koneksi, $koneksi);
+  $Result1 = mysql_query($updateSQL, $koneksi) or die(mysql_error());
+
+  $updateGoTo = "#";
   if (isset($_SERVER['QUERY_STRING'])) {
-    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
-    $insertGoTo .= $_SERVER['QUERY_STRING'];
+    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+    $updateGoTo .= $_SERVER['QUERY_STRING'];
   }
-  header(sprintf("Location: %s", $insertGoTo));
+  header(sprintf("Location: %s", $updateGoTo));
 }
+
+$colname_editdata = "-1";
+if (isset($_GET['no_pasien'])) {
+  $colname_editdata = $_GET['no_pasien'];
+}
+mysql_select_db($database_koneksi, $koneksi);
+$query_editdata = sprintf("SELECT * FROM pasien WHERE no_pasien = %s", GetSQLValueString($colname_editdata, "int"));
+$editdata = mysql_query($query_editdata, $koneksi) or die(mysql_error());
+$row_editdata = mysql_fetch_assoc($editdata);
+$totalRows_editdata = mysql_num_rows($editdata);
 ?>
-<!DOCTYPE html >
+<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-	<link type="text/css" href="js/themes/base/ui.all.css" rel="stylesheet" />
+<link rel='stylesheet' type='text/css' href='../library/bootstrap/bootstrap.css'/>
+<link rel='stylesheet' type='text/css' href='../library/bootstrap/style.css'/>
+<title>SI Klinik - Admin</title>
+<link type="text/css" href="js/themes/base/ui.all.css" rel="stylesheet" />
 
 
 <script type="text/javascript">
@@ -84,68 +93,82 @@ $target = "files/";
 </head>
 
 <body>
-<form action="<?php echo $editFormAction; ?>" method="post" enctype="multipart/form-data" name="form1" id="form1">
-  <table style='margin-top:10px;background:transparent' class="table">
-    <tr valign="baseline">
-      <td width="127" align="right" nowrap="nowrap"><div align="left">Nama Pasien </div></td>
-      <td width="483"><input required type="text" name="nm_pasien" value="" size="32" class="span9" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Jenis Kelamin </div></td>
-      <td><label for="j_kel"></label>
-        <select name="j_kel" id="j_kel">
-          <option value="Pria">Pria</option>
-          <option value="Wanita">Wanita</option>
-      </select></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Agama </div></td>
-      <td><label for="agama"></label>
-        <select name="agama" id="agama">
-          <option value="islam">Islam</option>
-          <option value="kristen">Kristen</option>
-          <option value="budha">Budha</option>
-          <option value="hindu">Hindu</option>
-      </select></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Alamat </div></td>
-      <td><input required type="text" name="alamat" value="" size="32" class="span9" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Tanggal Lahir </div></td>
-      <td><input required type="text" name="tgl_lhr" size="32" class="span9" id="tanggal" />
-      </td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Usia </div></td>
-      <td><input required type="text" name="usia" value="" size="32" class="span9" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Nomor Telephone </div></td>
-      <td><input required type="text" name="no_tlp" value="" size="32" class="span9" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Nama Kepala Keluarga </div></td>
-      <td><input required type="text" name="nm_kk" value="" size="32" class="span9" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right"><div align="left">Hubungan Keluarga </div></td>
-      <td><label for="hub_kel"></label>
-        <select name="hub_kel" id="hub_kel">
-          <option value="Anak Kandung">Anak Kandung</option>
-          <option value="Tidak Kandung">Lainnya</option>
-      </select></td>
-    </tr>
-
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">&nbsp;</td>
-      <td><input class="btn btn-success" type="submit" value="Tambahkan Data" /></td>
-    </tr>
-  </table>
-  <label for="textfield"></label>
-  <input type="hidden" name="MM_insert" value="form1" />
-</form>
+<div class='container-fluid' style='margin-top:20px;'>
+<?php include("navbar.php"); ?>
+	<div class='row-fluid'>
+	<?php include("sidebar.php"); ?>
+    <div class="span9">
+    <div class='well' fixed center;'>
+		<b>Sistem Informasi Klinik</b>
+		<p style='margin-top:10px'>
+	  <form method="post" name="form1" id="form1">
+	    <input type="hidden" name="no_pasien" />
+	  </form>
+      <form action="<?php echo $editFormAction; ?>" method="post" name="form2" id="form2">
+        <table style='margin-top:10px;background:transparent' class="table">
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Nomor Pasien</td>
+            <td><?php echo $row_editdata['no_pasien']; ?></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Nama Pasien</td>
+            <td><input required type="text" name="nm_pasien" value="<?php echo htmlentities($row_editdata['nm_pasien'], ENT_COMPAT, 'utf-8'); ?>" size="32" class="span9" /></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Jenis Kelamin</td>
+            <td><select name="j_kel">
+              <option value="pria" <?php if (!(strcmp("pria", htmlentities($row_editdata['j_kel'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Pria</option>
+              <option value="wanita" <?php if (!(strcmp("wanita", htmlentities($row_editdata['j_kel'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Wanita</option>
+            </select></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Agama Pasien</td>
+            <td><select name="agama">
+              <option value="islam" <?php if (!(strcmp("islam", htmlentities($row_editdata['agama'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Islam</option>
+              <option value="Kristen" <?php if (!(strcmp("Kristen", htmlentities($row_editdata['agama'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Kristen</option>
+              <option value="budha" <?php if (!(strcmp("budha", htmlentities($row_editdata['agama'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Budha</option>
+              <option value="hindu" <?php if (!(strcmp("hindu", htmlentities($row_editdata['agama'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Hindu</option>
+            </select></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Alamat Pasien</td>
+            <td><input required type="text" name="alamat" value="<?php echo htmlentities($row_editdata['alamat'], ENT_COMPAT, 'utf-8'); ?>" size="32" class="span9" /></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Tanggal Lahir</td>
+            <td><input required type="text" name="tgl_lhr" id="tanggal" value="<?php echo htmlentities($row_editdata['tgl_lhr'], ENT_COMPAT, 'utf-8'); ?>" size="32" class="span9" /></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Usia</td>
+            <td><input required type="text" name="usia" value="<?php echo htmlentities($row_editdata['usia'], ENT_COMPAT, 'utf-8'); ?>" size="32" class="span9" /></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Nama Kepala Keluarga</td>
+            <td><input required type="text" name="nm_kk" value="<?php echo htmlentities($row_editdata['nm_kk'], ENT_COMPAT, 'utf-8'); ?>" size="32" class="span9" /></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">Hubungan Keluarga</td>
+            <td><select name="hub_kel">
+              <option value="Anak Kandung" <?php if (!(strcmp("Anak Kandung", htmlentities($row_editdata['hub_kel'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Anak Kandung</option>
+              <option value="Tidak Kandung" <?php if (!(strcmp("Tidak Kandung", htmlentities($row_editdata['hub_kel'], ENT_COMPAT, 'utf-8')))) {echo "SELECTED";} ?>>Lainnya</option>
+            </select></td>
+          </tr>
+          <tr valign="baseline">
+            <td nowrap="nowrap" align="right">&nbsp;</td>
+            <td><input type="submit" value="Simpan" class="btn btn-success" />
+            <input name="button" type="submit" class="btn btn-warning" id="button" value="Hapus" /></td>
+          </tr>
+        </table>
+        <input type="hidden" name="MM_update" value="form2" />
+        <input type="hidden" name="no_pasien" value="<?php echo $row_editdata['no_pasien']; ?>" />
+      </form>
+      <p>&nbsp;</p>
 <p>&nbsp;</p>
+    </div>
+    </div>
+</div>
 </body>
 </html>
+<?php
+mysql_free_result($editdata);
+?>
